13affbbd169266e0f551602cf37eabd9ac8513c1
Yadullah Duman <yadullah.duman@gmail.com>
2024-12-02 19:07:13 +0100

    day 1 part 2

--- day01/main.ts
+++ day01/main.ts
@@ -1,26 +1,53 @@
 import { abs } from "https://deno.land/x/math@v1.1.0/mod.ts";
 
 const example = await Deno.readTextFile("./day01/example.txt");
+const example2 = await Deno.readTextFile("./day01/example2.txt");
 const locations = await Deno.readTextFile("./day01/input.txt");
+const locations2 = await Deno.readTextFile("./day01/input2.txt");
 
-let firstList = [];
-let secondList = [];
-const regex = /(\d+)\s+(\d+)/;
+function getLists(input) {
+  const regex = /(\d+)\s+(\d+)/;
+  const firstList = [];
+  const secondList = [];
 
-locations.split("\n").forEach((line) => {
-  const match = line.match(regex);
-  if (match) {
-    firstList.push(match[1]);
-    secondList.push(match[2]);
+  input.split("\n").forEach((line) => {
+    const match = line.match(regex);
+    if (match) {
+      firstList.push(match[1]);
+      secondList.push(match[2]);
+    }
+  });
+
+  return { firstList, secondList };
+}
+
+function solve1() {
+  const { firstList, secondList } = getLists(locations);
+  firstList.sort();
+  secondList.sort();
+
+  const accDistance = firstList.reduce((acc, el1, i) => {
+    const el2 = secondList[i];
+    return acc + Number(abs(el1 - el2));
+  }, 0);
+
+  console.log(`Distance result: ${accDistance}`);
+}
+
+function solve2() {
+  const { firstList, secondList } = getLists(locations2);
+  const freqMap = new Map<string, number>();
+  for (const el of secondList) {
+    freqMap.set(el, (freqMap.get(el) || 0) + 1);
   }
-});
 
-firstList.sort();
-secondList.sort();
+  const accSimScore = firstList.reduce((acc, el) => {
+    const count = freqMap.get(el) || 0;
+    return acc + Number(el) * count;
+  }, 0);
 
-const accDistance = firstList.reduce((acc, el1, i) => {
-  const el2 = secondList[i];
-  return acc + Number(abs(el1 - el2));
-}, 0);
+  console.log(`Sim score result: ${accSimScore}`);
+}
 
-console.log(accDistance);
+solve1();
+solve2();

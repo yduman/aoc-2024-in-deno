5e9b1e32f0874d012985f3d6ae916a441600c704
Yadullah Duman <yadullah.duman@gmail.com>
2024-12-03 18:41:30 +0100

    day 3 part 2

--- day03/main.ts
+++ day03/main.ts
@@ -1,18 +1,38 @@
 const example = await Deno.readTextFile("./day03/example.txt");
+const example2 = await Deno.readTextFile("./day03/example2.txt");
 const input = await Deno.readTextFile("./day03/input.txt");
+const input2 = await Deno.readTextFile("./day03/input2.txt");
 
-const memRegex = /(mul\(\d+,\d+\))/g;
+const memRegex = /(mul\(\d+,\d+\))|(do\(\))|(don't\(\))/g;
 const instrRegex = /(\d+)\,(\d+)/;
 
-const instructions = input.match(memRegex) as string[];
-// console.log(instructions);
+const instructions = input2.match(memRegex) as string[];
+
+function removeDisabledInstructions(instructions: string[]) {
+  const result: string[] = [];
+  let skip = false;
+
+  for (const instruction of instructions) {
+    if (instruction === "don't()") {
+      skip = true;
+    } else if (instruction === "do()") {
+      skip = false;
+      continue;
+    }
+
+    if (!skip) {
+      result.push(instruction);
+    }
+  }
+
+  return result;
+}
 
 function multiply(instructions: string[]) {
   return instructions.reduce((acc, instr) => {
     const matches = instr.match(instrRegex);
-    // console.log(matches)
     return acc + Number(matches![1]) * Number(matches![2]);
   }, 0);
 }
 
-console.log(multiply(instructions));
+console.log(multiply(removeDisabledInstructions(instructions)));

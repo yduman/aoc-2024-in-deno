4d191d6b817870f79339a52dfe5326328d4e682e
Yadullah Duman <yadullah.duman@gmail.com>
2024-12-04 21:01:18 +0100

    day 4 part 2

--- day04/main.ts
+++ day04/main.ts
@@ -50,8 +50,7 @@ function getWordSearchDiagonals(matrix: string[][]): string[] {
   return diagonals;
 }
 
-function search(wordSearch: string[]) {
-  const regex = /(?=(XMAS|SAMX))/g;
+function search(wordSearch: string[], regex: RegExp) {
   let matches = 0;
   for (const row of wordSearch) {
     for (const _ of row.matchAll(regex)) {
@@ -61,13 +60,57 @@ function search(wordSearch: string[]) {
   return matches;
 }
 
+function isM(x: string) {
+  return x === "M";
+}
+
+function isS(x: string) {
+  return x === "S";
+}
+
+function isXMAS(matrix: string[][], x: number, y: number) {
+  const topl_bottomr = isM(matrix[x - 1][y - 1]) && isS(matrix[x + 1][y + 1]);
+  const topr_bottoml = isM(matrix[x - 1][y + 1]) && isS(matrix[x + 1][y - 1]);
+  const bottoml_topr = isM(matrix[x + 1][y - 1]) && isS(matrix[x - 1][y + 1]);
+  const bottomr_topl = isM(matrix[x + 1][y + 1]) && isS(matrix[x - 1][y - 1]);
+
+  return [topl_bottomr, topr_bottoml, bottoml_topr, bottomr_topl].filter((x) =>
+    x === true
+  ).length === 2;
+}
+
+function searchP2(matrix: string[][]) {
+  const rowLen = matrix.length;
+  const colLen = matrix[0].length;
+  let count = 0;
+
+  for (let i = 1; i < rowLen - 1; i++) {
+    for (let j = 1; j < colLen - 1; j++) {
+      if (matrix[i][j] === "A" && isXMAS(matrix, i, j)) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
 const wordSearchRows = getWordSearchRows(input);
 const wordSearchMatrix = getWordSearchMatrix(input);
+
 const wordSearchTransposed = transpose(wordSearchMatrix);
 const wordSearchDiagonals = getWordSearchDiagonals(wordSearchMatrix);
 
-const horizontalMatches = search(wordSearchRows);
-const verticalMatches = search(wordSearchTransposed);
-const diagonalMatches = search(wordSearchDiagonals);
+const regexP1 = /(?=(XMAS|SAMX))/g;
+const horizontalMatches = search(wordSearchRows, regexP1);
+const verticalMatches = search(wordSearchTransposed, regexP1);
+const diagonalMatches = search(wordSearchDiagonals, regexP1);
 
-console.log(horizontalMatches + verticalMatches + diagonalMatches);
+const xmasMatches = searchP2(wordSearchMatrix);
+
+console.log(
+  "P1 Result:",
+  horizontalMatches + verticalMatches + diagonalMatches,
+);
+
+console.log("P2 Result: ", xmasMatches);
